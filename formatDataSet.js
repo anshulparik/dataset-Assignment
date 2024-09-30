cf82a7753bc7acff6781ebb7abdbad23ad8a808b
Anshul Parik <anshulparik@gmail.com>
2024-10-01 00:16:36 +0530

    feat: initial setup dataset imported, formatDataSet logic added

---
+++ formatDataSet.js
@@ -0,0 +1,19 @@
+import { dataset } from "./dataSet.js";
+
+export const formatDataSet = () => {
+  const arrayDataSet = dataset?.trim()?.split("\n");
+  const usableDataSet = arrayDataSet.slice(1);
+
+  const data = usableDataSet?.map((item) => {
+    const [date, sku, unitPrice, quantity, totalPrice] = item.split(",");
+    return {
+      date,
+      sku,
+      unitPrice,
+      quantity,
+      totalPrice,
+    };
+  });
+  
+  return data;
+};
