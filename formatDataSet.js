f2dce1d842f183c8c55cf90175be014a6f3b31d9
Anshul Parik <anshulparik@gmail.com>
2024-10-01 01:25:39 +0530

    feat: findMostPopularItem function added

--- formatDataSet.js
+++ formatDataSet.js
@@ -2,7 +2,7 @@ import { dataset } from "./dataSet.js";
 
 export const formatDataSet = () => {
   const arrayDataSet = dataset?.trim()?.split("\n");
-  const usableDataSet = arrayDataSet.slice(1);
+  const usableDataSet = arrayDataSet?.slice(1);
 
   const data = usableDataSet?.map((item) => {
     const [date, sku, unitPrice, quantity, totalPrice] = item.split(",");
--- queries.js
+++ queries.js
@@ -1,14 +1,12 @@
 import { formatDataSet } from "./formatDataSet.js";
 
 const data = formatDataSet();
-// console.log(data, "==== Dataset")
+
 
 // 1. Total sales of the store.
 const calculateTotalSales = () => {
   return data.reduce((total, item) => total + item.totalPrice, 0);
 };
-// console.log(calculateTotalSales());
-
 
 
 
@@ -25,4 +23,44 @@ const calculateMonthWiseSales = () => {
   });
   return monthWiseSales;
 };
-// console.log(calculateMonthWiseSales());
+
+
+
+// 3. Most popular item (most quantity sold) in each month.
+const findMostPopularItem = () => {
+  const itemSalesPerMonth = {};
+
+  data.forEach((item) => {
+    const date = new Date(item?.date);
+    const month = date.toISOString().slice(0, 7);
+
+    if (!itemSalesPerMonth[month]) {
+      itemSalesPerMonth[month] = {};
+    }
+
+    if (!itemSalesPerMonth[month][item?.sku]) {
+      itemSalesPerMonth[month][item?.sku] = 0;
+    }
+
+    itemSalesPerMonth[month][item?.sku] =
+      itemSalesPerMonth[month][item?.sku] + item?.quantity;
+  });
+
+
+  const mostPopularItem = {};
+  for (const month in itemSalesPerMonth) {
+    let quantity = 0,
+      popularItem = 0;
+
+    for (const item in itemSalesPerMonth[month]) {
+      if (itemSalesPerMonth[month][item] > quantity) {
+        quantity = itemSalesPerMonth[month][item];
+        popularItem = item;
+      }
+    }
+
+    mostPopularItem[month] = popularItem;
+  }
+
+  return mostPopularItem;
+};
